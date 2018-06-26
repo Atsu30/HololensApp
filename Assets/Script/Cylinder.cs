26bc2f114d1565ae0d25d4fbd33f6797b5b16b56
atsushi.mori <atsushi.mori@ar.c.titech.ac.jp>
2018-06-26 21:47:09 +0900

    First commit.

---
+++ Assets/Script/Cylinder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class Cylinder : MonoBehaviour, IFocusable
+{
+    public Material focusOn;
+    public Material focusOut;
+
+    public void OnFocusEnter()
+    {
+        GetComponent<Renderer>().material = focusOn;
+    }
+
+    public void OnFocusExit()
+    {
+        GetComponent<Renderer>().material = focusOut;
+    }
+}
